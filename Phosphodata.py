c51535d3109c3eabb803aecc1a83ab88ca121585
joachimsiaw <joachim.siasw@gu.se>
2018-05-09 12:43:42 +0300

    restructure of code

--- Phosphodata.py
+++ Phosphodata.py
@@ -10,12 +10,12 @@ Read file
 Look into column 2 which corresponds to Fold change
      if Fold change < 0
        then print fold change and corresponding gene name in column 3 
-   	if Fold change < -1.5
-	then print gene name and corresponding fold change and save as List_fold_change<-1.5
+     if Fold change < -1.5
+       print gene name and corresponding fold change and save as List_fold_change<-1.5
 
-if Fold change > 0
+     if Fold change > 0
        then print fold change and corresponding gene name in column 3 
-   	if Fold change > 1.5
-	then print gene name and corresponding fold change and save as List_fold_change>1.5
+     if Fold change > 1.5
+	    then print gene name and corresponding fold change and save as List_fold_change>1.5
 
 
