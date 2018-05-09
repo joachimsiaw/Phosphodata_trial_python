6cbcc58b1a9b658424310eca9f598ad12cbda8c4
joachimsiaw <joachim.siasw@gu.se>
2018-05-09 12:32:43 +0300

    first commit

---
+++ Phosphodata.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+Pseudocode
+Read file
+Look into column 2 which corresponds to Fold change
+     if Fold change < 0
+       then print fold change and corresponding gene name in column 3 
+   	if Fold change < -1.5
+	then print gene name and corresponding fold change and save as List_fold_change<-1.5
+
+if Fold change > 0
+       then print fold change and corresponding gene name in column 3 
+   	if Fold change > 1.5
+	then print gene name and corresponding fold change and save as List_fold_change>1.5
+
+
